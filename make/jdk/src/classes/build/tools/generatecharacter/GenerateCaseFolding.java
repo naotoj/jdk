4dc6ba6cbbb3f138ae3ba5d32a49459099c2bce4
Naoto Sato <naoto.sato@oracle.com>
2026-02-19 17:19:22 -0800

    Removed BMP assumption in the casefold buildtool

--- make/jdk/src/classes/build/tools/generatecharacter/GenerateCaseFolding.java
+++ make/jdk/src/classes/build/tools/generatecharacter/GenerateCaseFolding.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -90,7 +90,6 @@ public class GenerateCaseFolding {
     private static long foldingToLong(String[] folding) {
         int cp = Integer.parseInt(folding[0], 16);
         long value = (long)Integer.parseInt(folding[1], 16);
-//        if (!Character.isSupplementaryCodePoint(cp) && folding.length != 2) {
         if (folding.length != 2) {
             var shift = 16;
             for (int j = 2; j < folding.length; j++) {
