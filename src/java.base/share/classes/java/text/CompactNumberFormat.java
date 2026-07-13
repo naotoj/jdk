ccae3ae9146f506bbf9c0791f5eeb23255ba3ab3
Naoto Sato <naoto.sato@oracle.com>
2026-07-13 11:15:44 -0700

    CLDR-14420 Update plural e to c (#5870)

--- src/java.base/share/classes/java/text/CompactNumberFormat.java
+++ src/java.base/share/classes/java/text/CompactNumberFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1296,7 +1296,7 @@ public final class CompactNumberFormat extends NumberFormat {
     }
 
     // Patterns for plurals syntax validation
-    private static final String EXPR = "([niftvwe])\\s*(([/%])\\s*(\\d+))*";
+    private static final String EXPR = "([niftvwec])\\s*(([/%])\\s*(\\d+))*";
     private static final String RELATION = "(!?=)";
     private static final String VALUE_RANGE = "((\\d+)\\.\\.(\\d+)|\\d+)";
     private static final String CONDITION = EXPR + "\\s*" +
